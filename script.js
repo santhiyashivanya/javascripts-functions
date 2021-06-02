8d810ab2fb3c52b5e916b16aa7fe5dc63f104738
santhiya2001 <santhiyar383@gmail.com>
2021-06-02 17:48:22 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,78 @@
+function wish()
+{
+console.log("hello santhiya");
+}
+
+wish()
+function naming(name)
+{
+    console.log(`hello ${name}`);
+}
+
+naming("san")
+naming("santhiya")
+function add (a,b){
+    console.log(a+b);
+}
+
+add(10,10)
+
+let a = parseInt(prompt("a:"));
+let b = parseInt(prompt("b:"));
+
+function sub(a,b){
+console.log(a-b);
+
+}
+
+  //sub(a,b)
+
+function sqrt (number){
+    return sqrt=number*number;
+}
+
+result=sqrt(99);
+console.log(result); 
+//sub(a,b)
+let p="please";
+function commanding(){
+    let q= "wait";
+    console.log(p);
+    console.log(q);
+    console.log(p+"  "+q);
+
+if(true)  r= "santhiya";{
+console.log(r);
+console.log(p+"  "+q +"  "+r);
+}
+}
+console.log(p);
+commanding()
+
+console.log("san");
+let san;
+
+let best1=3;
+console.log(best1);
+//var best1
+
+let best3;
+console.log(best3);
+
+console.log(best4);
+var best4=9;
+
+function settledown(num){
+    console.log(num);
+    num=num-1;
+    if(num>=0){
+        settledown(num);
+    }
+}
+
+settledown(20)
+
+
+
+
+
